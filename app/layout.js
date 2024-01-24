6b060c79f80e5d7c6d86ce7f1ef68e41af7b2e72
whevalmeze <princegbefavor@gmail.com>
2024-01-24 23:56:57 +0100

    add svg files

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[#fafafa]">
       <body className={plus.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Sidebar from "./component/Sidebar";
+import Navbar from "./component/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="m-0">
+      <Sidebar/>
+      <div>
+        <Navbar/>  
+      </div>
     </main>
   );
 }
